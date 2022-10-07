e15d0a6d2a6fc8c00872ee9cd3327963d2c3bf16
Wilmer Estrada <edfon2020@gmail.com>
2022-10-06 21:00:19 -0600

    Se añaden mejoras al sistema de biblioteca

--- Proyecto Final Python Basico/Mini_Libreria_Python.py
+++ Proyecto Final Python Basico/Mini_Libreria_Python.py
@@ -2,7 +2,6 @@ import json
 
 ########################################################################################################
 ####### Abre el archivo json en donde tenemos nuestra base de datos ####################################
-
 def abrirBD():
     with open("../Datos/data.json") as data_base:
         data = json.load(data_base)
@@ -10,11 +9,41 @@ def abrirBD():
 ######### Visualizar seccion
 def visualizarSeccion(datos):
     cont = 1
-
     print("Las secciones disponibles son las siguientes")
     for seccion in datos.keys():
         print(f"{cont} - {seccion}")
         cont+=1
+    menu = """
+            Que desea hacer? 
+            1- Listar Libros disponibles
+            2- Agregar un libro nuevo
+            3- Salir
+           
+           Elija una opcion por favor: 
+    """
+    while True:
+        opcion = input(menu)
+
+        if opcion == '3':
+            break
+        elif opcion == '1':
+            seccion = input("Escriba la seccion que desea visualizar ")
+            visualizarLibros(datos,seccion)
+        else:
+            print("La opcion seleccionada no es correcta.")
+
+#####  Visualizar libros
+def visualizarLibros(data, seccion):
+
+    seccion = data[seccion]
+
+    for libro in seccion:
+        print("Libro :")
+        print(f" Autor: {libro['Autor']}")
+        print(f" Titulo: {libro['Titulo']}")
+        print(f" Editorial: {libro['Editorial']}")
+        print(f" Area: {libro['Area']}")
+
 
 ############################################################################################################
 def run():
