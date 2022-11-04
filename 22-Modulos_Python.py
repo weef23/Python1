4b0b36241123887f03c9a05a7dd4a4a9266364d0
Wilmer Estrada <weef23@hotmail.com>
2022-11-04 11:16:51 -0600

    Ejemplo de modulos en Python

---
+++ 22-Modulos_Python.py
@@ -0,0 +1,8 @@
+### De esta forma podemos llamar a nuestro modulo arias
+from Modulos import Modulo_Areas as ma ## Podemos definir un Alias para invocarlo mas facilmente
+
+### Veamos el ejemplo
+area_circulo = ma.circulo(2)
+print(f"El area de un circulo es {area_circulo}")
+area_circulo = ma.circulo(5)
+print(f"El area de un circulo es {area_circulo}")
---
+++ Modulos/Modulo_Areas.py
@@ -0,0 +1,9 @@
+############### EJEMPLO DE UN MODULO EN PYTHON ####################################
+import math
+
+pi = math.pi ## Constante Pi
+
+## Calcula el area de un circulo
+def circulo(radio):
+    area = pi*(radio**2)
+    return  area
