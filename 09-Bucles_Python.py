32788d3645acba36cb83e49a497ce80509c60aa4
Wilmer Estrada <weef23@hotmail.com>
2022-11-11 19:34:12 -0600

    Ejemplo con for y if resumido

--- 09-Bucles_Python.py
+++ 09-Bucles_Python.py
@@ -18,12 +18,31 @@ def ejemploCicloFor():
         potencia = 2 ** i
         print(f"2 elevado a {i} es igual a {potencia}")
 
+def ejemploBusquedaFor():
+    listaValores = [1,4,8,12,16,20,24,28,32]
+    ## Digite el numero que desea buscar
+    busqueda = int(input("Digite el numero que desea buscar => "))
+
+    contador = 0
+    for numero in listaValores:
+        if numero == busqueda:
+            print("Numero encontrado con exito")
+            contador += 1
+        else:
+            continue
+    else:
+        print(f"Se encontraron {contador} resultados") if contador > 0 else print("No se encontro ningun resultado")
+
+
 def run():
     print("Ejecucion de ciclo While \n")
     ejemploBucleWhile()
 
     print("Ejecucion de ciclo For \n")
     ejemploCicloFor()
+
+    print("Ejemplo de busqueda usando ciclos for:\n")
+    ejemploBusquedaFor()
 
 
 if __name__ == "__main__":
